d7a1bfeb6397ea46964c4be0b24d6d2a7154aab4
riftana <60580246+riftana@users.noreply.github.com>
2020-02-24 13:53:48 +0600

    Add files via upload

---
+++ labFinal24/signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Step1</title>
+</head>
+<body>
+	<form method="post" action="upload.php">
+	Name:<input type="text" name="name"><br>
+	Email:<input type="email" name="email"><br>
+	Gender:<input type="radio" name="gender" value="male">Male
+	        <input type="radio" name="gender" value="female">Female
+	        <input type="radio" name="gender" value="others">Others
+	        <br><br>
+	        Password:<input type="password" name="password"><br>
+	<input type="submit" name="submit">
+	
+</form>
+</body>
+</html>
